8ebb4846a28ef91a7155fbef3232aa3d545520df
vignetties <vignetties@gmail.com>
2020-07-09 12:51:32 +0530

    Update get-info.py

--- get-info.py
+++ get-info.py
@@ -1,63 +1,167 @@
----
-- name: "Load allianz.server.conf"
-  read_allianz_server_conf:
-  register: server_conf
+from __future__ import (absolute_import, division, print_function)
 
-- name: Create a new host or update an existing host's info
-  local_action:
-    module: zabbix_tag
-    server_url: "{{ zabbix_server }}"
-    login_user: tu_ansible
-    login_password: pwd4ansible
-    host_name: "{{inventory_hostname |replace ('.srv.allianz', '')}}"
-    stage: "{{server_conf.content.STAGE}}"
-    mandant: "{{server_conf.content.MANDANT}}"
-    servertype: "{{server_conf.content.SERVERTYPE}}"
-    jbossprofiles: "{{server_conf.content.JBOSSPROFILES | default('')}}"
-    tomcatprofiles: "{{server_conf.content.TOMCATPROFILES| default('')}}"
-    asifportoffsets: "{{server_conf.content.ASIF_PORTOFFSETS| default('')}}"
+__metaclass__ = type
 
-#- name: Update zabbix configuration
-#  cron:
-#    name: "update zabbix configuration"
-#    minute: "*/15"
-#    job: "cd /etc/zabbix; if ! git diff --quiet remotes/origin/HEAD; then git pull; service zabbix-agent restart; fi"
+import os
+import requests
+import subprocess
+import datetime
+import time
+import json
+import urllib3
+from ansible.module_utils._text import to_bytes
+from ansible.plugins.callback import CallbackBase
 
-- name: "Update git repo"
-  shell: cd /etc/zabbix && git fetch origin && git reset --hard origin/master && git checkout master && git pull
-  environment:
-    http_proxy: http://fr003-e2-svr.zone2.proxy.allianz:8090
-    https_proxy: http://fr003-e2-svr.zone2.proxy.allianz:8090
-  become: true
+def deployment_info(self, stats):
+    app = self.extra_vars.get("APP", None)
+    version = self.extra_vars.get("VERSION", None)
+    tenant = self.extra_vars.get("TENANT", None)
+    jobid = self.extra_vars.get("tower_job_id", None)
+    launchtype = self.extra_vars.get("tower_job_launch_type", None)
+    templatename = self.extra_vars.get("tower_job_template_name", None)
+    ppmurl = self.extra_vars.get("PPMURL", None)
+    stage = self.extra_vars.get("STAGE", None)
+    username = self.extra_vars.get("tower_user_name", None)
+    requestid = self.extra_vars.get("REQUESTID", None)
+    deployment_info = self.extra_vars.get("DEPLINFO", None)
+    ansible_hostname = self.extra_vars.get("ansible_hostname", None)
+    now = datetime.datetime.now()
+    epoch = time.time()
+    epoch = int(epoch)
+    hosts = sorted(stats.processed.keys())
+    hostsInScope = []
+    
+    statusname = "Successful"
+    for h in hosts:
+        hostsInScope.append(h)
+        s = stats.summarize(h)
+        errors = sum([s['failures'], s['unreachable']])
+        if errors > 0:
+            statusname = "Failed"
 
-- name: "Re-create zabbix_agentd.conf"
-  become: true
-  template: 
-    src: ../../zabbix/templates/zabbix_agentd.conf.j2 
-    dest: /etc/zabbix/zabbix_agentd.conf
-    mode: 644
+    #only send post request when status is success
+    if statusname == "Successful":
+        nonprod = ['DEV', 'TEST', 'TEXT', 'PREPROD', 'MNTN', 'QA', 'EDU', 'JIT']
+        if stage == 'PROD':
+            base_url = "https://awp-elk-prod.srv.allianz/grafana/api/annotations/graphite"
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJrIjoiN1habmF3Z3dZYXVzNjZBU0RDcUxnRDZHMnBWU3QwWFYiLCJuIjoiYWRtaW4iLCJpZCI6MX0='
+            }
+        elif stage in nonprod:
+            base_url = "https://awp-elk-nonprod.srv.allianz/grafana/api/annotations/graphite"
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJrIjoiZ0cyY3JYbDVCNWlJcmwyWG5LWUlvTUJSYzRITDhmaVciLCJuIjoiQWRtaW4iLCJpZCI6MX0='
+            }
+        else:
+            print("Wrong STAGE defined")
+    
+        if app.startswith('ABS_')  or app.startswith('CISL_MEDIATOR') or app.startswith('CISL_SEARCH') or app.startswith('DISPATCH_SCHEDULER'):
+            if app.startswith('ABS_') and app != "ABS_MAG":
+                platform = "ABS "
+                component = app.split("_", 2)
+                component = component[2]
+            elif (app == 'CISL_MEDIATOR') or app.startswith('CISL_SEARCH') or (app == 'DISPATCH_SCHEDULER'):
+                platform = "ASIF "
+                component = app
+            elif app == "ABS_MAG":
+                platform = "ABS "
+                component = app
+        
+            what = platform + stage + " Deployment"
+            data = platform + "V" + version
+            tags = platform + component + " " + stage
 
-- stat:
-    path: /usr/lib/systemd/system/zabbix-agent.service
-  register: service_file
+            if "-" in tenant:
+                ten = tenant.split("-",2)
+                ten = ten[1]
+            else:
+                ten = tenant.split(" ",2)
+                if ten[1] == "Europe":
+                    ten = "EU"
+                elif ten[1] == "EU":
+                    ten = "EU"
 
-- name: install customized zabbix-agent systemd unit file
-  template:
-    src: ../../zabbix/templates/zabbix-agent-systemd.service
-    dest: /usr/lib/systemd/system/zabbix-agent.service
-    mode: 644
-  when: service_file.stat.exists
+            payload = {
+            'what': what,
+            'tags': [platform,component,stage],
+            'when': epoch,
+            'data': data
+            }
+            payload = json.dumps((payload))
+            cafile='/etc/ssl/certs/ca-bundle.crt'
+            certs = requests.certs.where()
 
-- name: start zabbix-agent
-  systemd:
-    state: started
-    name: zabbix-agent 
-    daemon_reload: yes
-  when: service_file.stat.exists
+            if (ten == "EU") or (ten == "Europe"):
+                #print("Data: %s %s %s" % (base_url, payload, headers))
+                response = requests.post(base_url, data=payload, verify=False, headers=headers)    
 
-- name: restart zabbix-agent
-  service:
-    name: zabbix-agent
-    state: restarted
-    enabled: true
-  become: true
+                count = 1
+
+                if response.status_code == 200:
+                    print("Data successfully sent to Grafana.")
+                else:
+                    while count <= 5:
+                        #print("ReturnCode: %s" % response.status_code)
+                        print("Request %i" % count)
+                        response = requests.post(base_url, data=payload, verify=False, headers=headers)
+                        if response.status_code == 200:
+                            print("Data successfully sent to Grafana.")
+                            break
+                        else:
+                            count = count + 1
+                    if count > 5:
+                        print("Sending data to Grafana failed 5 times - sent mail to azp-devops.")
+                        cmd = ("mail -s \"Deployment info failed\" -r root@LX-ANSIBLE01 azp-devops@allianz.at <<< \"https://ansible.aeat.allianz.at/#/jobs/playbook/%s \nStatus code: %s\"" % (jobid, response.status_code))
+                        subprocess.call([cmd], shell=True)
+
+    urllib3.PoolManager(
+        cert_reqs = 'CERT_REQUIRED',
+        ca_certs = '/etc/pki/tls/certs/ca-bundle.crt'
+    )
+
+class CallbackModule(CallbackBase):
+
+    def __init__(self, display=None):
+        super(CallbackModule, self).__init__(display)
+        self.results = []
+        self.play = None
+        self.extra_vars = None
+        self.failure = False
+
+    def _new_play(self, play):
+        return {
+            'play': {
+                'name': play.name,
+                'id': str(play._uuid)
+            },
+            'tasks': []
+        }
+
+    def _new_task(self, task):
+        return {
+            'task': {
+                'name': task.name,
+                'id': str(task._uuid)
+            },
+            'hosts': {}
+        }
+
+    def v2_playbook_on_play_start(self, play):
+        var_mgr = play.get_variable_manager()
+        self.play = play
+        self.extra_vars = var_mgr.extra_vars
+        self.results.append(self._new_play(play))
+
+    def v2_playbook_on_stats(self, stats):
+        deployment_info(self, stats)
+
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        self.results[-1]['tasks'].append(self._new_task(task))
+    
+    def v2_runner_on_ok(self, result, **kwargs):
+        deployment_info(self, result)
+        host = result._host
+        self.results[-1]['tasks'][-1]['hosts'][host.name] = result._result
+        notify(self.results);
